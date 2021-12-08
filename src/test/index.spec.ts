917c29f786f783a59480af0d9591584b4dc7c41b
Brice Fernandes <brice@fractallambda.com>
2021-12-08 22:05:14 +0000

    Update to use new config options

--- src/test/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import { app } from "../app";
-
-describe("GET /", () => {
-    it("should return 200 OK", () => {
-        return request(app).get("/")
-            .expect(200);
-    });
-});
